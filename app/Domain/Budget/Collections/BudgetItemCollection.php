5cee1a465f07829a77f995251db31bc904fcb57e
James Allen <jacques2186@yahoo.com>
2022-05-24 09:26:29 -0400

    Add BudgetItemCollection

---
+++ app/Domain/Budget/Collections/BudgetItemCollection.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Budget\Collections;
+
+use Budget\Actions\ConvertIntegerToDollarsAction;
+use Budget\Models\BudgetItem;
+use Illuminate\Database\Eloquent\Collection;
+
+class BudgetItemCollection extends Collection
+{
+    public function withPlannedAmountInDollars(): self
+    {
+        return $this->map(function (BudgetItem $budget) {
+            $budget['planned_in_dollars'] = (new ConvertIntegerToDollarsAction($budget->planned_amount))->execute();
+            return $budget;
+        });
+    }
+}
--- app/Domain/Budget/Models/BudgetItem.php
+++ app/Domain/Budget/Models/BudgetItem.php
@@ -2,6 +2,7 @@
 
 namespace Budget\Models;
 
+use Budget\Collections\BudgetItemCollection;
 use Database\Factories\BudgetItemFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -15,6 +16,11 @@ class BudgetItem extends Model
     public static function newFactory()
     {
         return new BudgetItemFactory();
+    }
+
+    public function newCollection(array $models = []): BudgetItemCollection
+    {
+        return new BudgetItemCollection($models);
     }
 
     public function category()
